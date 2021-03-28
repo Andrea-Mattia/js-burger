eed5d5718f1784a34daebbfd0ef94d1d8e4e3022
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-03-28 19:02:05 +0200

    add clickable add for checkbox

--- js/script.js
+++ js/script.js
@@ -11,9 +11,22 @@ var burgName = document.getElementById('name');
 var ingredient = document.getElementsByClassName('ingredient-checkbox');
 var totPrice = document.getElementById('price');
 var coupon = document.getElementById('coupon');
+var addBtn = document.getElementsByClassName('ingredient-add');
 
 // Settings
 var discount = ['123456ABCDEF', '789101GHIJKL', '111213MNOPQR']
+
+// Add cliccabili per cambiare lo status della checkbox degli ingredienti
+for (var i = 0; i < addBtn.length; i++) {
+    var add = addBtn[i];
+    add.addEventListener('click', function () {
+        // uso this per indicare l'elemento cliccato al momento
+        // uso previousElementSibiling per èrendere l'elemento direttamente prima
+        var thisCheckbox = this.previousElementSibling;
+        // // faccio in modo che se la checkbox e checked passa ad unchecked e viceversa
+        thisCheckbox.checked = ! thisCheckbox.checked;
+    });
+}
  
 // Calcolo del prezzo
 // aggiungo event listener per capire quando si clicca dul bottone
